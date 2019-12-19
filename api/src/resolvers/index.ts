35f08d09090b61afb510d1519ec6f73065a4d1ec
AmoDinho <u14284783@tuks.co.za>
2019-12-19 15:17:51 +0200

    referenced getFanCams in index.ts

--- api/src/resolvers/index.ts
+++ api/src/resolvers/index.ts
@@ -1,7 +1,8 @@
-import { hello } from "./query";
+import { hello, getFanCams } from "./query";
 import { IResolvers } from "graphql-tools/dist/Interfaces"
 export const resolvers: IResolvers = {
     Query: {
-        hello: (root: any, args: any, context: any) => hello(args, context)
+        hello: (root: any, args: any, context: any) => hello(args, context),
+        getFanCams: (root: any, args, any, context: any) => getFanCams(args, context)
     }
 }
