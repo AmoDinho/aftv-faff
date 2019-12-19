37c084a812bf29aef5f330f1042ee4ab4cceca4c
AmoDinho <u14284783@tuks.co.za>
2019-12-19 15:31:58 +0200

    added function

--- api/src/resolvers/query.ts
+++ api/src/resolvers/query.ts
@@ -1,7 +1,18 @@
+import { resolvers } from "."
+
 export const hello = (args: any, context: any) => {
     return "Your GraphQL API is now LIVE!🎈 "
 }
 
 export const getFanCams = (args: any, context: any) => {
+    const PATH_BASE: string = 'https://www.googleapis.com/youtube/v3/search?part=snippet&'
+    const CHANNEL_ID: string = 'channelId=UCBTy8j2cPy6zw68godcE7MQ'
+    const KEY = `&key=${process.env.YOUTUBE_API_KEY}`
+
+    const fanCams: object = fetch(`${PATH_BASE}${CHANNEL_ID}${KEY}`)
+        .then(response => console.log(response.json))
+        .catch(error => console.log(error))
+
+
     return null
 }
