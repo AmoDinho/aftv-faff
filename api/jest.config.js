9b78108d006a188e866f4aef361af60972f34ba5
AmoDinho <u14284783@tuks.co.za>
2020-01-06 15:14:41 +0200

    trying to get the tests to work

--- api/jest.config.js
+++ api/jest.config.js
@@ -1,4 +1,18 @@
 module.exports = {
+  bail: true,
   preset: 'ts-jest',
   testEnvironment: 'node',
+  verbose: true,
+  transform: {
+    '^.+\\.ts?$': 'ts-jest'
+  },
+  testRegex: "(/tests/.*|(\\.|/)(test)\\.(js?|ts?)$",
+  moduleFileExtensions: ["ts", "js"],
+  globals: {
+    'ts-jest': {
+      diagnostics: {
+        pathRegex: /\.(spec|test)\.ts$/,
+      }
+    }
+  }
 };
