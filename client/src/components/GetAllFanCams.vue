c76854afab08756463adf86cdc7d3fd3d979a603
AmoDinho <u14284783@tuks.co.za>
2020-01-07 12:29:25 +0200

    got the query componet working

--- client/src/components/GetAllFanCams.vue
+++ client/src/components/GetAllFanCams.vue
@@ -1,20 +1,37 @@
 <template>
-  <h1>All AFTV Fan Cams</h1>
+  <div>
+    <h1>All AFTV Fan Cams</h1>
+    <ApolloQuery
+    :query="require('../graphql/GetFanCams.gql')"
+    >
+<template v-slot="{result: {loading,error,data}}">
+<div v-if="loading">
+Loading...
+    </div>
+    <div v-else-if="error">
+An error is here
+    </div>
+    <div v-else-if="data">
+<div v-for="cam in data">
+      {cam.ID}
+      {cam.title}
+      {cam.url}
+    </div>
+    </div>
 </template>
-<script lang="ts">
-import Vue from "vue";
-import GetFanCams from "../graphql/GetFanCams.gql";
-export default Vue.extend({
+        </ApolloQuery>
+ 
+    
+  </div>
+</template>
+<script >
+
+export default {
   name: "GetAllFanCams",
   data() {
     return {
       allFanCams: []
     };
-  },
-  apollo: {
-    allFanCams: {
-      query: GetFanCams
-    }
   }
-});
+};
 </script>
