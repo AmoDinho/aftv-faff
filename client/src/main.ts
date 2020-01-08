4fc167d0c100738b9334697f0d0e84b3a6d4974c
AmoDinho <u14284783@tuks.co.za>
2020-01-08 11:53:27 +0200

    removed the require statement

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,8 +1,8 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-const createProvider = require('./vue-apollo')
-//import { createProvider } from './vue-apollo'
+//const createProvider = require('./vue-apollo')
+import { createProvider } from './vue-apollo'
 
 Vue.config.productionTip = false;
 
