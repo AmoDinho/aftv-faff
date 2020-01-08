e9070a11838dce1c844c995a211f6c77d2a8f2c6
AmoDinho <u14284783@tuks.co.za>
2020-01-08 11:27:04 +0200

    fixed declaration file issue

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,7 +1,8 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
-import { createProvider } from './vue-apollo'
+const createProvider = require('./vue-apollo')
+//import { createProvider } from './vue-apollo'
 
 Vue.config.productionTip = false;
 
